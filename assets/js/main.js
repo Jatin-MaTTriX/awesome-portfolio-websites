801a9dc981ad4d9999719d114abc109b84664405
Jatin Sharma <72266203+Jatin-MaTTriX@users.noreply.github.com>
2021-01-15 00:11:22 +0530

    github icon hover effect
    black color is added while hovering over github icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,8 +32,8 @@ let header = $(`
 <nav class="fixed-top black">
 <div class="nav-wrapper">
     <a href="#!" class="brand-logo"><img src="assets/images/John-Doe1.png"></a>
-    <a href="#" data-target="mobile-demo" class="white-text sidenav-trigger"><i
-            class="material-icons">menu</i></a>
+    <a href="#" data-target="mobile-demo" class="white-text sidenav-trigger">
+    <i class="material-icons">menu</i></a>
     <ul class="right hide-on-med-and-down">
         <li><a class="white-text" href="index.html">Home</a></li>
         <li><a class="white-text" href="experience.html">Experience</a></li>
